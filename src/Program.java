39a99cb28066aaeb995dfbc9fb5094b7c360d953
slogemann1 <sllogemann1@gmail.com>
2021-05-19 22:22:01 +0200

    Added reading some command line arguments (untested)

--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,65 @@
-import org.json.JSONObject;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
 
 class Program {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        String outfilePath = null;
+        String action = null; // Possible values: doRequest, getHomepage
+        String query = null;
+
+        // Lese argumente
+        for(String arg : args) {
+            if(arg.startsWith("unique_file_path=")) {
+                outfilePath = arg.split("unique_file_path=")[1];
+            }
+            else if(arg.startsWith("action=")) {
+                action = arg.split("action=")[1];
+            }
+            else if(arg.startsWith("query=")) {
+                String[] argSplit = arg.split("query=");
+                query = argSplit.length > 1 ? argSplit[1] : "";
+            }
+        }
+
+        if(outfilePath == null || action == null) {
+            returnCgiError(outfilePath);
+        }
+
+        // Erstelle Objekte
+        RequestHandler rq = new RequestHandler();
+        PageGenorator pg = new PageGenorator(rq);
+
+        // Führe die gegebene aktion aus
+        String fileText = null;
+        try {
+            if(action.equals("getHomepage")) {
+                fileText = pg.generateHomepage();
+            }
+            else {
+                returnCgiError(outfilePath);
+            }
+        }
+        catch (AppException e) {
+            writeToFile(outfilePath, e.toString() + ": " + e.getDetails());
+            System.exit(42);
+        }
+
+        writeToFile(outfilePath, fileText);
+    }
+
+    static void returnCgiError(String nullableFilename) throws IOException {
+        String errMsg = "Invalid arguments from server";
+        if(nullableFilename != null) writeToFile(nullableFilename, errMsg);
+        else System.out.println(errMsg);
+        System.exit(42); // CGI Fehler
+    }
+
+    static void writeToFile(String filename, String text) throws IOException {
+        FileWriter fw = new FileWriter(filename);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(text);
+        bw.close();
     }
 }
 
